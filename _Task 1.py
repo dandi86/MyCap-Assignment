4c18483ffd2b5aca1acddc9a6bbb75e969465a66
Shilpa Shivarudra <104411748+dandi86@users.noreply.github.com>
2022-06-22 22:02:30 +0530

    Add files via upload

---
+++ _Task 1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python Programming
+
+# In[1]:
+
+
+print("Hello World1")
+
+
+# In[2]:
+
+
+print("Welcome to Python Programming world")
+
+
+# In[ ]:
+
+
+
+
