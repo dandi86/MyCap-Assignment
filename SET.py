abc51b557aecb4a96c4cc9aef85988f9e303db6d
Shilpa Shivarudra <104411748+dandi86@users.noreply.github.com>
2022-06-22 22:25:07 +0530

    Add files via upload

---
+++ SET.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[28]:
+
+
+E= {0,1, 2, 3, 4, 5,7}
+N = {2, 4 ,6,8}
+
+
+# In[24]:
+
+
+print("The UNION of E and N is:",E|N)
+
+
+# In[25]:
+
+
+print("The INTERSECTION of E and N is:",E&N)
+
+
+# In[29]:
+
+
+print("The SYMMETRIC DIFFERENCE  of E and N is:",E^N)
+
+
+# In[30]:
+
+
+print("The DIFFERENCE of E and N is:",E-N)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
